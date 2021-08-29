5d68b42b692e19b465556606afbaa9fee69da169
dm-evstafiev <d.evstafiev@crystals.ru>
2021-08-29 03:49:33 +0300

    5

--- src/main/java/org/example/springtest/MusicPlayer.java
+++ src/main/java/org/example/springtest/MusicPlayer.java
@@ -3,11 +3,36 @@ package org.example.springtest;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
+    public MusicPlayer(){}
+
     public MusicPlayer(Music music){
         this.music = music;
     }
 
     public void playMusic() {
         System.out.println("Play: " + music.getSong());
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
